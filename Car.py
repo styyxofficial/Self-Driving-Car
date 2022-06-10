95f205739cab7e9ded0279c79f4ecd2652c001b0
Aryan Singh <asingh.ryan@gmail.com>
2022-06-10 07:20:28 -0700

    Created moving box

--- Car.py
+++ Car.py
@@ -1,29 +1,21 @@
-import math
-
-
 class Car:
-
-    def __init__(self, canvas, x, y, len, wid, xVelocity, yVelocity, color):
-        """
-        @param x: center x position of the rectangle
-        @param y: center y position of the rectangle
-        @param len: length of the rectangle
-        @param wid: width of the rectangle
-        """
-
-        self.canvas = canvas
-
-        x_0 = x - len/2
-        y_0 = y - wid/2
-        x_1 = x + len/2
-        y_1 = y + wid/2
-
-        self.image = canvas.create_rectangle(x_0, y_0, x_1, y_1, fill=color)
-        self.xVelocity = xVelocity
-        self.yVelocity = yVelocity
-
-    def move(self, time):
-        # function of time
-        velocity = math.sin(time)
-
-        self.canvas.move(self.image, self.xVelocity, self.yVelocity)
+    def __init__(self, image, x, y):
+        self.image = image
+        self.x = x
+        self.y = y
+    
+    def draw(self, screen):
+        screen.blit(self.image, (self.x, self.y))
+        
+    
+    def move_up(self):
+        self.y -= 1
+    
+    def move_down(self):
+        self.y += 1
+    
+    def move_left(self):
+        self.x -= 1
+    
+    def move_right(self):
+        self.x += 1
--- Moving_Car.py
+++ Moving_Car.py
@@ -1,23 +1,65 @@
-from tkinter import *
-import time
+import pygame
+from pygame.locals import *
 from Car import Car
+import sys
+
+def on_init():
+    pygame.init()
+
+on_init()
 
 
-# if in 1 second the car has not changed its position that much, kill the car
-window = Tk()
+    
+_running = True
+screen = pygame.display.set_mode((500,500), pygame.HWSURFACE | pygame.DOUBLEBUF)
+screen.fill("WHITE")
+pygame.key.set_repeat(16)
 
-WIDTH = 700
-HEIGHT = 700
-
-canvas = Canvas(window, width=WIDTH, height=HEIGHT)
-canvas.pack()
-
-car_1 = Car(canvas, 350, 350, 50, 70, 0, 0, "red")
+# Load game assests
+car_image = pygame.image.load("images/rectangle.png").convert()
+car_1 = Car(car_image, 250, 250)
 
 
-while True:
-    car_1.move(3)
-    window.update()
-    time.sleep(0.032)
 
-window.mainloop()
+def on_event(event):
+    if event.type == QUIT:
+        on_cleanup()
+        
+        
+    if pygame.key.get_pressed()[K_DOWN]:
+        screen.fill("WHITE")
+        car_1.move_down()
+        
+    if pygame.key.get_pressed()[K_UP]:
+        screen.fill("WHITE")
+        car_1.move_up()
+    
+    if pygame.key.get_pressed()[K_LEFT]:
+        screen.fill("WHITE")
+        car_1.move_left()
+    
+    if pygame.key.get_pressed()[K_RIGHT]:
+        screen.fill("WHITE")
+        car_1.move_right()
+
+def on_loop():
+    pass
+    
+def on_render():
+    pygame.display.flip()
+    car_1.draw(screen)
+    pygame.display.flip()
+ 
+def on_cleanup():
+    pygame.display.quit()
+    pygame.quit()
+    sys.exit()
+ 
+    
+ 
+while(_running):
+    for event in pygame.event.get():
+        on_event(event)
+    on_loop()
+    on_render()
+on_cleanup()
