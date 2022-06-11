2ebf8debbad96cdcf1ce1d4d7a6742e221c586f9
Aryan Singh <asingh.ryan@gmail.com>
2022-06-11 05:16:01 -0700

    friction addition

--- Car.py
+++ Car.py
@@ -1,29 +1,112 @@
 import pygame
 from pygame.locals import *
 import math
+
+
 class Car:
     def __init__(self, image, x, y, angle):
         self.image = image
         self.x = x
         self.y = y
         self.angle = angle
-    
+        self.distance_traveled = 0.0
+
+        # Acceleration that is controlled by arrow keys
+        self.linear_acceleration = 0.0
+        self.rotational_acceleration = 0.0
+
+        # Affected by time and acceleration
+        self.linear_velocity = 0.0
+        self.rotational_velocity = 0.0
+
     def draw(self, screen):
+
+        time_unit = 2
+
+        self.check_max_acceleration()
+
+        self.linear_velocity += time_unit * self.linear_acceleration
+        self.rotational_velocity += time_unit * self.rotational_acceleration
+
+        self.check_max_velocity()
+
+        self.angle += self.rotational_velocity * time_unit
+        self.distance_traveled += self.linear_velocity * time_unit
+
+        # Direction times distance to travel
+        self.x += math.cos((self.angle) * (math.pi/180)) * (self.linear_velocity * time_unit)
+        self.y -= math.sin((self.angle) * (math.pi/180)) * (self.linear_velocity * time_unit)
+
+        self.check_bounds()
+
         img = pygame.transform.rotate(self.image, self.angle)
-        screen.blit(img, (self.x, self.y))
-        
-    
-    def move_forward(self):
-        self.x += math.cos((self.angle+90) * (math.pi/180))
-        self.y -= math.sin((self.angle+90) * (math.pi/180))
-    
-    def move_backward(self):
-        self.x -= math.cos((self.angle+90) * (math.pi/180))
-        self.y += math.sin((self.angle+90) * (math.pi/180))
-    
+
+        # screen.blit(img, (self.x, self.y))
+        screen.blit(img, img.get_rect(center=self.image.get_rect(topleft=(self.x, self.y)).center))
+
+    def move_forward(self, time_unit):
+        time_unit = 1
+        self.linear_acceleration += 2
+
+    def move_backward(self, time_unit):
+        time_unit = 1
+        self.linear_acceleration -= 2
+
     def move_left(self):
-        self.angle += 1
-        
-    
+        self.rotational_acceleration += 1
+
     def move_right(self):
-        self.angle -= 1
+        self.rotational_acceleration -= 1
+
+    def friction(self, time_unit):
+
+        # By multiplying with the velocity, we get an effective terminal velocity
+        # Higher number will allow the car to move faster
+        terminal_velocity = 2
+
+        if self.linear_velocity > 0:
+            self.linear_acceleration -= self.linear_velocity
+        elif self.linear_velocity < 0:
+            self.linear_acceleration -= self.linear_velocity
+        else:
+            self.linear_acceleration -= 0
+
+        if self.rotational_velocity > 0:
+            self.rotational_acceleration -= 1
+        elif self.rotational_velocity < 0:
+            self.rotational_acceleration += 1
+        else:
+            self.rotational_acceleration -= 0
+
+    def __getattribute__(self, __name: str):
+        return object.__getattribute__(self, __name)
+
+    def check_max_acceleration(self):
+        # if (self.linear_acceleration > 5):
+        #     self.linear_acceleration = 5
+        # if (self.linear_acceleration < -5):
+        #     self.linear_acceleration = -5
+        if (self.rotational_acceleration > 2):
+            self.rotational_acceleration = 2
+        if (self.rotational_acceleration < -2):
+            self.rotational_acceleration = -2
+
+    def check_max_velocity(self):
+        if (self.linear_velocity > 10):
+            self.linear_velocity = 10
+        if (self.linear_velocity < -10):
+            self.linear_velocity = -10
+        if (self.rotational_velocity > 5):
+            self.rotational_velocity = 5
+        if (self.rotational_velocity < -5):
+            self.rotational_velocity = -5
+
+    def check_bounds(self):
+        if (self.x < 0):
+            self.x = 1
+        if (self.x > 500-self.image.get_size()[0]):
+            self.x = 499 - self.image.get_size()[0]
+        if (self.y < 0):
+            self.y = 0
+        if (self.y > 500-self.image.get_size()[1]):
+            self.y = 499 - self.image.get_size()[1]
--- Moving_Car.py
+++ Moving_Car.py
@@ -3,59 +3,71 @@ from pygame.locals import *
 from Car import Car
 import sys
 
+
 def on_init():
     pygame.init()
+
 
 on_init()
 
 
-    
 _running = True
-screen = pygame.display.set_mode((500,500), pygame.HWSURFACE | pygame.DOUBLEBUF)
+screen = pygame.display.set_mode(
+    (500, 500), pygame.HWSURFACE | pygame.DOUBLEBUF)
 screen.fill("WHITE")
-pygame.key.set_repeat(16)
+
+# Number of ms in 1 time unit
+# Needed for acceleration
+time_unit = 16
+pygame.key.set_repeat(time_unit)
 
 # Load game assests
-car_image = pygame.image.load("images/rectangle.png").convert()
+# Car image used from : https://github.com/NeuralNine/ai-car-simulation/blob/master/car.png
+car_image = pygame.image.load("images/car.png").convert_alpha()
+car_image = pygame.transform.scale(car_image, (100, 50))
 car_1 = Car(car_image, 250, 250, 0)
-
 
 
 def on_event(event):
     if event.type == QUIT:
         on_cleanup()
-        
-        
+
     if pygame.key.get_pressed()[K_DOWN]:
         screen.fill("WHITE")
-        car_1.move_backward()
-        
+        car_1.move_backward(time_unit)
+
     if pygame.key.get_pressed()[K_UP]:
         screen.fill("WHITE")
-        car_1.move_forward()
-    
+        car_1.move_forward(time_unit)
+
     if pygame.key.get_pressed()[K_LEFT]:
         screen.fill("WHITE")
         car_1.move_left()
-    
+
     if pygame.key.get_pressed()[K_RIGHT]:
         screen.fill("WHITE")
         car_1.move_right()
 
+    # pygame.time.delay(16)
+
+
 def on_loop():
     pass
-    
+
+
 def on_render():
+    screen.fill("WHITE")
+    car_1.friction(time_unit)
     car_1.draw(screen)
     pygame.display.flip()
- 
+
+
 def on_cleanup():
     pygame.display.quit()
     pygame.quit()
     sys.exit()
- 
-    
- 
+
+
 while(_running):
     for event in pygame.event.get():
         on_event(event)
