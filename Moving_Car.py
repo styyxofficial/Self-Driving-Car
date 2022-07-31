6cf5349d6d88904bcb5575ed5249dffc6b12ef93
Aryan Singh <asingh.ryan@gmail.com>
2022-07-31 04:03:49 -0700

    configured neural network to be as simple as poss

--- Moving_Car.py
+++ Moving_Car.py
@@ -1,42 +1,26 @@
 from turtle import back
 import pygame
 from pygame.locals import *
+from sklearn import neural_network
 from Car import Car
 import sys
 import neat
 import time
 import multiprocessing
 import visualize
+from NeuralNetwork import NeuralNetwork
+import copy
+import numpy as np
 
 
 # Load Config
-# screen_width = 1920
-# screen_height = 1080
+screen_width = 1920
+screen_height = 1080
 
-# _running = True
+_running = True
 
-# FPS = 60
-# fpsClock = pygame.time.Clock()
-
-# # Load game assests
-# # map as background image
-
-# # Car image used from : https://github.com/NeuralNine/ai-car-simulation/blob/master/car.png
-
-
-# screen = pygame.display.set_mode((screen_width, screen_height), pygame.HWSURFACE | pygame.DOUBLEBUF)
-
-# car_image = pygame.image.load("images/car.png").convert_alpha()
-# car_image = pygame.transform.scale(car_image, (100, 50))
-# background_image = pygame.image.load("images/map.png").convert_alpha()
-
-screen_width = None
-screen_height = None
-
-_running = None
-
-FPS = None
-fpsClock = None
+FPS = 60
+fpsClock = pygame.time.Clock()
 
 # Load game assests
 # map as background image
@@ -44,11 +28,31 @@ fpsClock = None
 # Car image used from : https://github.com/NeuralNine/ai-car-simulation/blob/master/car.png
 
 
-screen = None
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.HWSURFACE | pygame.DOUBLEBUF)
 
-car_image = None
-car_image = None
-background_image = None
+car_image = pygame.image.load("images/car.png").convert_alpha()
+car_image = pygame.transform.scale(car_image, (100, 50))
+background_image = pygame.image.load("images/map.png").convert_alpha()
+
+# screen_width = None
+# screen_height = None
+
+# _running = None
+
+# FPS = None
+# fpsClock = None
+
+# Load game assests
+# map as background image
+
+# Car image used from : https://github.com/NeuralNine/ai-car-simulation/blob/master/car.png
+
+
+# screen = None
+
+# car_image = None
+# car_image = None
+# background_image = None
 
 
 
@@ -90,25 +94,72 @@ def on_cleanup():
     sys.exit()
 
 
-def run_simulation(genomes, config):
+def run_simulation(num_cars, run_iter):
     # Empty Collections For Nets and Cars
-    nets = []
-    cars = []
-
+    
+    
     
     # For All Genomes Passed Create A New Neural Network
-    for i, g in genomes:
-        net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets.append(net)
-        g.fitness = 0
-
-        cars.append(Car(car_image, 881, 800, 0))
+    # for i in range(num_cars):
+        
+    #     #print(nn.layers[0][0])
+    #     nets[i] = nn
+        
+        
 
     # timeout = time.time() + 60*5   # 5 minutes from now
     # timeout = time.time() + 15   # 10 seconds from now
+    best_layer, best_fitness = (None, 0)
+    generation = 0
 
+    for _ in range(run_iter):
+        
+        nets = [None] * num_cars
+        cars = [None] * num_cars
+    
+        for i in range(num_cars):
+            nn = NeuralNetwork(layers=best_layer)
+            
+            if best_fitness>90 | generation>50:
+                nn.set_mutations(mutation_rate=0.08, mutation_power=0.0001)
+                        
+            if best_layer!=None:
+                nn.mutate2()
+                
+            nets[i] = nn
+            cars[i] = Car(car_image, 881, 800, 0)
+            
+        nets[0] = NeuralNetwork(layers=best_layer)
+        
+        print("Generation:", generation)
+        # print("Num cars", len(cars))
+        # print(nets[2].layers)
+        best_net_idx, best_fitness = method2(nets, cars)
+        
+        # Randomly initialize cars until they have a good start (minimum fitness to begin mutations)
+        if best_fitness > 10:
+            best_layer = nets[best_net_idx].layers
+        
+        
+            
+        # for i in range(num_cars):
+        #     nets[i].mutate3(best_net.layers)
+            
+        # nets[0] = best_net
+        
+        #print(len(nets))
+        #print(best_net)
+        generation += 1
+    
+    print(best_layer)
+    
+        
+
+def method2(nets, cars):
+    
     timeout = time.time() + 15  # 15 seconds after current time
 
+    #print(nets)
     while(_running):
 
         # End the game when the X is pressed
@@ -123,23 +174,23 @@ def run_simulation(genomes, config):
 
         cars_alive = 0
         for i, car in enumerate(cars):
-
+            # print(car.is_alive, i)
             if car.is_alive:
                 cars_alive += 1
-                genomes[i][1].fitness = car.get_fitness()
 
-                output = nets[i].activate(car.get_data())
+                output = nets[i].calculate(car.get_data())
                 # This needs to be tested
-                choice = output.index(max(output))
+                choice = output.argmax()
                 if choice == 0:
                     car.move_forward()
                 elif choice == 1:
                     car.move_backward()
                 elif choice == 2:
                     car.move_left()
-                else:
+                elif choice == 3:
                     car.move_right()
 
+                car.update(screen)
                 car.draw(screen)
         pygame.display.flip()
 
@@ -155,175 +206,23 @@ def run_simulation(genomes, config):
         # on_loop()
         # on_render()
         fpsClock.tick(FPS)
-
-def eval_genome(genome, config):
-    net = neat.nn.FeedForwardNetwork.create(genome, config)
-    car = Car(car_image, 881, 800, 0)
-
-    timeout = time.time() + 15  # 15 seconds after current time
-        
-    while car.is_alive:
-        # End the game when the X is pressed
-        for event in pygame.event.get():
-            on_event(event)
-            
-        screen.blit(background_image, (0, 0))
-        
-        
-
-        output = net.activate(car.get_data())
-        # This needs to be tested
-        choice = output.index(max(output))
-        if choice == 0:
-            car.move_forward()
-        elif choice == 1:
-            car.move_backward()
-        elif choice == 2:
-            car.move_left()
-        else:
-            car.move_right()
-
-        car.update(screen)
-        car.draw(screen)
-        
-        genome.fitness = car.get_fitness()
-        pygame.display.flip()
-        
-        #fpsClock.tick(FPS)
-    return car.get_fitness()
-
-def eval_genome2(genomes, config):
     
-    global screen_width, screen_height, _running, FPS, fpsClock, screen, car_image, background_image
+    max_fitness = -1000000000
+    best_net_idx = 0
+    for i, car in enumerate(cars):
+        if car.get_fitness() > max_fitness:
+            max_fitness = car.get_fitness()
+            best_net_idx = i
     
-    screen_width = 1920
-    screen_height = 1080
-
-    _running = True
-
-    FPS = 60
-    fpsClock = pygame.time.Clock()
-
-    # Load game assests
-    # map as background image
-
-    # Car image used from : https://github.com/NeuralNine/ai-car-simulation/blob/master/car.png
-
-
-    screen = pygame.display.set_mode((screen_width, screen_height), pygame.HWSURFACE | pygame.DOUBLEBUF)
-
-    car_image = pygame.image.load("images/car.png").convert_alpha()
-    car_image = pygame.transform.scale(car_image, (100, 50))
-    background_image = pygame.image.load("images/map.png").convert_alpha()
-
-
-    # Empty Collections For Nets and Cars
-    nets = []
-    cars = []
-    fitness = []
+    print(max_fitness, best_net_idx)
     
-    # For All Genomes Passed Create A New Neural Network
-    for i, g in genomes:
-        net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets.append(net)
-        g.fitness = 0
-        fitness.append(0)
-        
-        cars.append(Car(car_image, 881, 800, 0))
-        
-    timeout = time.time() + 15  # 15 seconds after current time
-    #print(genomes)
-    #print(_running)
-    while _running:
-        cars_alive = 0
-        screen.blit(background_image, (0, 0))
-        
-        for i, car in enumerate(cars):
-            if car.is_alive:
-                cars_alive += 1
-                genomes[i][1].fitness = car.get_fitness()
-                fitness[i] = car.get_fitness()
-                
-                output = nets[i].activate(car.get_data())
-                # This needs to be tested
-                choice = output.index(max(output))
-                if choice == 0:
-                    car.move_forward()
-                elif choice == 1:
-                    car.move_backward()
-                elif choice == 2:
-                    car.move_left()
-                else:
-                    car.move_right()
-
-                car.update(screen)
-                car.draw(screen)
-        
-        pygame.display.flip()
-        #print(cars_alive)
-        if cars_alive == 0:
-            break
-        if time.time() > timeout:
-            break
-        
-        fpsClock.tick(FPS)
-    #print(fitness)
-    return fitness
+    return best_net_idx, max_fitness
 
 
 if __name__ == '__main__':
     on_init()
     
-    config_path = "config.txt"
-    config = neat.config.Config(neat.DefaultGenome,
-                                neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet,
-                                neat.DefaultStagnation,
-                                config_path)
-    
-    config_path2 = "config2.txt"
-    config2 = neat.config.Config(neat.DefaultGenome,
-                                neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet,
-                                neat.DefaultStagnation,
-                                config_path2)
-    
-    config_path3 = "config3.txt"
-    config3 = neat.config.Config(neat.DefaultGenome,
-                                neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet,
-                                neat.DefaultStagnation,
-                                config_path3)
-
-    config_path4 = "config4.txt"
-    config4 = neat.config.Config(neat.DefaultGenome,
-                                neat.DefaultReproduction,
-                                neat.DefaultSpeciesSet,
-                                neat.DefaultStagnation,
-                                config_path4)
-    
-    config_list = [config, config2, config3]
-    
-    # Create Population And Add Reporters
-    population = neat.Population(config_list)
-    population.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    population.add_reporter(stats)
-
-    # Run for up to 400 generations.
-    pe = neat.ParallelEvaluator(6, eval_genome2, maxtaskperchild=36*2)
-    winner = population.run(pe.evaluate, 200)
-    
-    # Draw the net
-    node_names = {0: 'Forward', 1: 'Backward', 2: 'Left', 3:'Right'}
-    visualize.draw_net(config, winner, True, node_names=node_names)
-    visualize.plot_stats(stats, ylog=False, view=True)
-    visualize.plot_species(stats, view=True)
-    
-    
-    # Run Simulation For A Maximum of 1000 Generations
-    #population.run(run_simulation, 10000)
-
+    run_simulation(15, 200)
 
 
     on_cleanup()
---
+++ NeuralNetwork.py
@@ -0,0 +1,95 @@
+import numpy as np
+import random
+import copy
+
+class NeuralNetwork:
+    def __init__(self, layers=None):
+        # Input Layer: 9
+        # Layer 1: 12
+        # Layer 2: 9
+        # Layer 3: 6
+        # Layer 4: 4
+        
+        self.mutation_init_mean = 0
+        self.mutation_init_sd = 1
+        self.mutation_rate = 0.3
+        self.mutation_power = 0.5
+
+        if layers == None:
+            self.layers = [
+                np.random.normal(self.mutation_init_mean, self.mutation_init_sd, size = (9, 5)), # Layer 1
+                np.random.normal(self.mutation_init_mean, self.mutation_init_sd, size = (5, 5)), # Layer 2
+                #np.random.normal(self.mutation_init_mean, self.mutation_init_sd, size = (9, 5)), # Layer 3
+                np.random.normal(self.mutation_init_mean, self.mutation_init_sd, size = (5, 5)) # Output Layer
+            ]
+        else:
+            self.layers = copy.deepcopy(layers)
+        
+    def relu(self, x):
+        return max(0, x)
+    
+    def calculate(self, input_layer):
+        current_calc = input_layer
+        
+        func = np.vectorize(self.relu)
+        
+        for layer in self.layers:
+            current_calc = current_calc @ layer
+            current_calc = func(current_calc)
+            
+        return current_calc
+    
+    def mutate(self):
+        temp_layers = self.layers
+        for layer in temp_layers:
+            nums_to_change = layer.size * self.mutation_rate
+            
+            for _ in range(round(nums_to_change)):
+                r = random.randint(0, layer.shape[0]-1)
+                c = random.randint(0, layer.shape[1]-1)
+                layer[r, c] = layer[r, c] + np.random.normal(0, self.mutation_power, 1)
+        return temp_layers
+    
+    # Working mutation model
+    def mutate2(self):
+        for layer in self.layers:
+            nums_to_change = layer.size * self.mutation_rate
+            
+            for _ in range(round(nums_to_change)):
+                r = random.randint(0, layer.shape[0]-1)
+                c = random.randint(0, layer.shape[1]-1)
+                layer[r, c] = layer[r, c] + np.random.normal(0, self.mutation_power, 1)
+    
+    
+        """
+        Mutate given layers and saves it to current layer
+        """
+    def mutate3(self, layers):
+        layers2 = copy.deepcopy(layers)
+        for layer in layers2:
+            nums_to_change = layer.size * self.mutation_rate
+            
+            for _ in range(round(nums_to_change)):
+                r = random.randint(0, layer.shape[0]-1)
+                c = random.randint(0, layer.shape[1]-1)
+                layer[r, c] = layer[r, c] + np.random.normal(0, self.mutation_power, 1)
+                
+        self.layers = layers2
+    
+    # similar to mutate 2, but it only mutates 1 thing at a time
+    def mutate4(self):
+        layer_to_mutate = random.randint(0, len(self.layers)-1)
+        r = random.randint(0, self.layers[layer_to_mutate].shape[0]-1)
+        c = random.randint(0, self.layers[layer_to_mutate].shape[1]-1)
+
+        self.layers[layer_to_mutate][r, c] = self.layers[layer_to_mutate][r, c] + np.random.normal(0, self.mutation_power, 1)
+                        
+    def set_layers(self, layers):
+        self.layers = layers
+    
+    def set_mutations(self, mutation_rate, mutation_power):
+        self.mutation_rate = mutation_rate
+        self.mutation_power = mutation_power
+                
+            
+    
